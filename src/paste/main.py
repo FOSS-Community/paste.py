484f070a6054c056718a1bcf7528ab9356008cf5
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-12-28 20:48:57 +0530

    :art: chore: Refactor code

--- src/paste/main.py
+++ src/paste/main.py
@@ -24,24 +24,26 @@ large_uuid_storage = []
 
 BASE_DIR = Path(__file__).resolve().parent
 
-templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
+templates = Jinja2Templates(directory=str(Path(BASE_DIR, "templates")))
 
 
 @app.post("/file")
-def post_as_a_file(file: UploadFile = File(...)):
+async def post_as_a_file(file: UploadFile = File(...)):
     try:
         uuid = generate_uuid()
         if uuid in large_uuid_storage:
             uuid = generate_uuid()
         path = f"data/{uuid}"
-        with open(path, 'wb') as f:
+        with open(path, "wb") as f:
             shutil.copyfileobj(file.file, f)
             large_uuid_storage.append(uuid)
             print(large_uuid_storage)
     except Exception:
         # return {"message": "There was an error uploading the file"}
-        raise HTTPException(detail="There was an error uploading the file",
-                            status_code=status.HTTP_403_FORBIDDEN)
+        raise HTTPException(
+            detail="There was an error uploading the file",
+            status_code=status.HTTP_403_FORBIDDEN,
+        )
     finally:
         file.file.close()
 
@@ -49,63 +51,68 @@ def post_as_a_file(file: UploadFile = File(...)):
 
 
 @app.get("/paste/{uuid}")
-def post_as_a_text(uuid):
+async def post_as_a_text(uuid):
     path = f"data/{uuid}"
     try:
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             return PlainTextResponse(f.read())
     except Exception as e:
         print(e)
-        raise HTTPException(detail="404: The Requested Resource is not found",
-                            status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(
+            detail="404: The Requested Resource is not found",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
 
 
 @app.get("/", response_class=HTMLResponse)
-def indexpage(request: Request):
+async def indexpage(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.delete("/paste/{uuid}", response_class=PlainTextResponse)
-def delete_paste(uuid):
+async def delete_paste(uuid):
     path = f"data/{uuid}"
     try:
         os.remove(path)
         return PlainTextResponse(f"File successfully deleted {uuid}")
     except FileNotFoundError:
-        raise HTTPException(detail="File Not Found",
-                            status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(
+            detail="File Not Found", status_code=status.HTTP_404_NOT_FOUND
+        )
     except Exception as e:
         raise HTTPException(
-            detail=f"The exception is {e}", status_code=status.HTTP_409_CONFLICT)
+            detail=f"The exception is {e}", status_code=status.HTTP_409_CONFLICT
+        )
 
 
 @app.get("/web", response_class=HTMLResponse)
-def web(request: Request):
+async def web(request: Request):
     return templates.TemplateResponse("web.html", {"request": request})
 
 
 @app.post("/web", response_class=PlainTextResponse)
-def web_post(content: str = Form(...)):
-    # print(content)
-    # return PlainTextResponse(content=content)
+async def web_post(content: str = Form(...)):
     try:
         file_content = content.encode()
         uuid = generate_uuid()
         if uuid in large_uuid_storage:
             uuid = generate_uuid()
         path = f"data/{uuid}"
-        with open(path, 'wb') as f:
+        with open(path, "wb") as f:
             f.write(file_content)
             large_uuid_storage.append(uuid)
     except Exception as e:
-        # return {"message": "There was an error uploading the file"}
         print(e)
-        raise HTTPException(detail="There was an error uploading the file",
-                            status_code=status.HTTP_403_FORBIDDEN)
+        raise HTTPException(
+            detail="There was an error uploading the file",
+            status_code=status.HTTP_403_FORBIDDEN,
+        )
 
-    return RedirectResponse(f"http://paste.fosscu.org/paste/{uuid}", status_code=status.HTTP_303_SEE_OTHER)
+    return RedirectResponse(
+        f"http://paste.fosscu.org/paste/{uuid}", status_code=status.HTTP_303_SEE_OTHER
+    )
 
 
 @app.get("/health", status_code=status.HTTP_200_OK)
-def health() -> dict[str, str]:
+async def health() -> dict[str, str]:
     return {"status": "ok"}
