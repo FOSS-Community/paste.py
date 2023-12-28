5dfc73fc080078247c1a1fbe67755353d4f2280e
Sam <asamadans@gmail.com>
2023-12-29 00:55:02 +0530

    added tests and refactored the targeted code

--- src/paste/utils.py
+++ src/paste/utils.py
@@ -1,9 +1,10 @@
 import random
 import string
 import os
+import sys
 
 
-def generate_uuid():
+def generate_uuid() -> str:
     # Combine uppercase letters, lowercase letters, and digits
     characters = string.ascii_letters + string.digits
 
@@ -13,6 +14,13 @@ def generate_uuid():
     return random_code
 
 
-def extract_extension(file_name):
+def extract_extension(file_name) -> str:
     _, extension = os.path.splitext(file_name)
     return extension
+
+
+def check_file_size_limit(file) -> bool:
+    # Check if the file size is less than 20 MB -> true else false
+    if sys.getsizeof(file) < 20_000_000:
+        return True
+    return False
---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture
+def check_file_size_limit(*args):
+    return True
